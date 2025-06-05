76dedc7419d7881d7098cd717ca3615f0f95fede
szymonpoterejkoinfe2 <sp306077@student.polsl.pl>
2025-06-05 23:29:44 +0200

    Initial project creation

---
+++ NegotiationAPI/NegotiationAPI.Api/Common/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿namespace NegotiationAPI.Api.Common
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+
+            return services;
+        }
+
+    }
+}
---
+++ NegotiationAPI/NegotiationAPI.Api/Program.cs
@@ -0,0 +1,31 @@
+using NegotiationAPI.Api.Common;
+using NegotiationAPI.Application.Common;
+using NegotiationAPI.Infrastructure.Common;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+builder.Services.AddPresentation();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+
+
+
+
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "NegotiationAPI"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
